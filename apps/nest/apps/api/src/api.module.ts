baa8b3e6af826b12f20924670486cdb070ef11ab
trungtran <baotrung06092003@gmail.com>
2025-04-15 23:13:08 +0700

    chore: rm unused code

--- apps/nest/apps/api/src/api.module.ts
+++ apps/nest/apps/api/src/api.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common'
 
 import { AuthModule } from '@app/auth/auth.module'
 import { CoreModule } from '@app/core/core.module'
-import { TodoModule } from '@app/todo/todo.module'
 
 @Module({
-  imports: [CoreModule, AuthModule, TodoModule],
+  imports: [CoreModule, AuthModule],
   controllers: [],
   providers: [],
 })
--- apps/nest/libs/todo/src/dtos/update-todo.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType, PickType } from '@nestjs/swagger'
-import { CreateTodoDto } from './create-todo.dto'
-
-class _UpdateTodoDto extends PickType(CreateTodoDto, ['title', 'description']) {}
-export class UpdateTodoDto extends PartialType(_UpdateTodoDto) {}
