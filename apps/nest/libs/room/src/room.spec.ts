df86ffc989e146bed0a6464da1d38b800bf2362b
trungtran <baotrung06092003@gmail.com>
2025-05-10 20:43:15 +0700

    spec: create new room

--- apps/nest/libs/room/src/room.spec.ts
+++ apps/nest/libs/room/src/room.spec.ts
@@ -2,6 +2,7 @@ import { TestContext, testHelper, UserContextTestType } from '@app/spec'
 import { INestApplication } from '@nestjs/common'
 import { PrismaService } from 'nestjs-prisma'
 import { RoomModule } from './room.module'
+import { CreateRoomDto } from './dtos/create-room.dto'
 
 describe('RoomSpec', () => {
   let tc: TestContext
@@ -22,8 +23,39 @@ describe('RoomSpec', () => {
     await tc.clean()
   })
 
-  it('should be defined', () => {
-    console.log('uc', uc)
-    expect(app).toBeDefined()
+  describe('Create', () => {
+    test('Create:NameIsRequired', async () => {
+      const payload: CreateRoomDto = {
+        name: '',
+        description: 'Test room',
+        capacity: 10,
+        location: 'Test location',
+        type: 'EVENT',
+        isAvailable: true,
+      }
+      const res = await uc.request((r) => r.post('/room')).send(payload)
+      expect(res).toBeBad('name should not be empty')
+    })
+
+    test('Create:Success', async () => {
+      const payload: CreateRoomDto = {
+        name: 'Test room',
+        description: 'Test room',
+        capacity: 10,
+        location: 'Test location',
+        type: 'EVENT',
+        isAvailable: true,
+      }
+      const res = await uc.request((r) => r.post('/room')).send(payload)
+      expect(res).toBeCreated()
+      expect(res.body).toMatchObject({
+        name: payload.name,
+        description: payload.description,
+        capacity: payload.capacity,
+        location: payload.location,
+        type: payload.type,
+        isAvailable: payload.isAvailable,
+      })
+    })
   })
 })
